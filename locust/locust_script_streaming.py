a26728c2541e374422ddad7056ec1942fcadca09
akshayar <akshaya.rawat@gmail.com>
2024-04-25 19:32:02 +0530

    Changes for custom load

--- locust/locust_script_streaming.py
+++ locust/locust_script_streaming.py
@@ -17,42 +17,6 @@ region = os.environ["REGION"]
 content_type = os.environ["CONTENT_TYPE"]
 payload_file = os.environ["PAYLOAD_FILE"]
 max_new_tokens = os.environ["MAX_NEW_TOKENS"]
-
-class TokenIterator:
-    def __init__(self, stream,request_meta):
-        self.byte_iterator = iter(stream)
-        self.request_meta = request_meta
-        self.buffer = io.BytesIO()
-        self.read_pos = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        logging.info("iterating")
-        try:
-            while True:
-                self.buffer.seek(self.read_pos)
-                line = self.buffer.readline()
-                if line and line[-1] == ord("\n"):
-                    self.read_pos += len(line) + 1
-                    full_line = line[:-1].decode("utf-8")
-                    line_data = json.loads(full_line.lstrip("data:").rstrip("/n"))
-                    return line_data["token"]["text"]
-                chunk = next(self.byte_iterator)
-                self.buffer.seek(0, io.SEEK_END)
-                self.buffer.write(chunk["PayloadPart"]["Bytes"])
-                if(len(line) != 0):
-                    string=line.decode('utf-8')
-                    self.first_token_time=time.perf_counter()
-                    self.request_meta["first_token_time"] = (time.perf_counter() - self.request_meta["start_perf_counter"]) * 1000
-                    logging.info(line)
-                    return line
-        except StopIteration:
-            self.request_meta["last_token_time"] = (time.perf_counter() - self.request_meta["start_perf_counter"]) * 1000
-            logging.info("The iterator is finished.")
-            raise StopIteration("done")
-
 class BotoClient:
     def __init__(self, host):
         # Consider removing retry logic to get accurate picture of failure in locust
@@ -79,18 +43,17 @@ class BotoClient:
             "exception": None,
         }
         start_perf_counter = time.perf_counter()
-        request_meta["start_perf_counter"] = start_perf_counter
 
         try:
-            logging.info("Sending request to SageMaker streaming")
             logging.debug(self.payload)
             response = self.sagemaker_client.invoke_endpoint_with_response_stream(
                 EndpointName=self.endpoint_name,
                 Body=self.payload,
                 ContentType=self.content_type,
             )
-            for token in TokenIterator(response["Body"]):
-                logging.info(token)
+            iterator = iter(response["Body"])
+            chunk = next(iterator)
+            logging.info(chunk)
         except Exception as e:
             request_meta["exception"] = e
 
