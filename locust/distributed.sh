d1ddbbf3f4007d3efabfc21832ec3110d0ff3f26
akshayar <akshaya.rawat@gmail.com>
2024-04-26 12:56:26 +0530

    Changes for custom load

--- locust/distributed.sh
+++ locust/distributed.sh
@@ -63,11 +63,11 @@ echo "SCRIPT: $SCRIPT"
 if $LOCUST_UI ; then
     locust -f $SCRIPT -H $ENDPOINT_NAME --master --expect-workers $WORKERS -u $USERS -t $RUN_TIME \
     --skip-log-setup \
-    --csv ${RESULT_FILE} --html ${HTML_RESULT_FILE} > tee $STD_OUT &
+    --csv ${RESULT_FILE} --html ${HTML_RESULT_FILE} >>  $STD_OUT &
 else
     locust -f $SCRIPT -H $ENDPOINT_NAME --master --expect-workers $WORKERS -u $USERS -t $RUN_TIME \
     --skip-log-setup \
-    --csv ${RESULT_FILE} --headless --html ${HTML_RESULT_FILE} > tee $STD_OUT &
+    --csv ${RESULT_FILE} --headless --html ${HTML_RESULT_FILE} >>  $STD_OUT &
 fi
 
 for (( c=1; c<=$WORKERS; c++ ))
