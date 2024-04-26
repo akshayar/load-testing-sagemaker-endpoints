5b22d75303daaa96ba1e7fd8a78843ef3603ea01
akshayar <akshaya.rawat@gmail.com>
2024-04-26 18:09:22 +0530

    Changes for custom load

--- locust/distributed.sh
+++ locust/distributed.sh
@@ -16,7 +16,7 @@ fi
 export PROPERTIES_FILE=$(default_if_empty config.sh $2)
 source "${PROPERTIES_FILE}"
 
-export USE_CASE=$(default_if_empty test "$USE_CASE")
+export USE_CASE=$(default_if_empty test "$3")
 export CONTENT_TYPE=$(default_if_empty application/json "${CONTENT_TYPE}")
 export RUN_TIME=$(default_if_empty 10m "${RUN_TIME}")
 export PAYLOAD_FILE=$(default_if_empty test.txt "${PAYLOAD_FILE}")
@@ -72,6 +72,7 @@ fi
 
 for (( c=1; c<=$WORKERS; c++ ))
 do
-    locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+    locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost \
+    --loglevel INFO --logfile ${LOG_FILE} &
 done
 
