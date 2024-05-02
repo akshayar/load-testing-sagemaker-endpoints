e612a265a5ab3b8f1e49803ca7c67ec167b96adc
akshayar <akshaya.rawat@gmail.com>
2024-05-02 16:28:38 +0530

    Changed for bedrock test

--- locust/locust_script_streaming.py
+++ locust/locust_script_streaming.py
@@ -82,7 +82,7 @@ class BotoClient:
             while True:
                 chunk = self.get_next_string(iterator)
                 logging.debug("Iterating response no %s : %s",str(i),chunk)
-                if chunk and chunk != '\n' and len(chunk) !=0:
+                if chunk and len(chunk) !=0:
                     logging.info("First token: %s",chunk)
                     return chunk
                 i+=1
