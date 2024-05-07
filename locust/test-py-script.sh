d10cca36e3258a563f2b014b769bf60ad22692c8
akshayar <akshaya.rawat@gmail.com>
2024-05-07 14:57:17 +0530

    Changed for bedrock test

--- locust/test-py-script.sh
+++ locust/test-py-script.sh
@@ -2,7 +2,7 @@
 #export HOST="meta.llama2-13b-chat-v1"
 export REGION=us-east-1
 export CONTENT_TYPE=application/json
-export PAYLOAD_FILE=test.txt
+export PAYLOAD_FILE=instruct.txt
 export MAX_NEW_TOKENS=500
 export HOST=$2
 python $1
