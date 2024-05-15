3bebc8ae81ec62a3605374b8a1acb7fa6d036cc5
akshayar <akshaya.rawat@gmail.com>
2024-05-15 19:28:28 +0530

    Changed for bedrock test

---
+++ locust/config-whisper-sagemaker-common.sh
@@ -0,0 +1,6 @@
+export CONTENT_TYPE=application/json
+export RUN_TIME=1m
+export REGION=us-east-1
+export LOCUST_UI=false
+export PAYLOAD_FILE=samples_jfk.wav
+export SCRIPT=locust_script_sagemaker_whisper.py
