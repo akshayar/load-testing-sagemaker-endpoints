d3d42818315e7eb4fe16091cd6c39ec9d2710535
akshayar <akshaya.rawat@gmail.com>
2024-06-20 21:01:22 +0530

    Script changes to support multiple models.

--- locust/bedrock/multi-run-config.sh
+++ locust/bedrock/multi-run-config.sh
@@ -3,6 +3,6 @@ export LOCUST_UI=false
 export MAX_NEW_TOKENS=100
 export PAYLOAD_FILE=../test-data/chat-1000-tokens.txt
 #export PAYLOAD_FILE=../test-data/chat-1000-tokens-anthropic.txt
-#export PAYLOAD_FILE=../test-data/chat-1000-tokens-claude.txt
+#export PAYLOAD_FILE=../test-data/chat-1000-tokens-anthropic.txt
 #export PAYLOAD_FILE=../test-data/chat-1000-tokens-mistral.txt
 #export PAYLOAD_FILE=../test-data/llama3-instruct-1000.txt
